4b28d33f126445517c9f1fe0a70185b7279b33fd
Jamen Marz <jamenmarz@gmail.com>
2015-11-18 13:00:16 -0700

    Add testing

---
+++ tests/unit.js
@@ -0,0 +1,37 @@
+'use strict';
+jest.dontMock('../lib/unit');
+
+describe('unit object', function(){
+  const Unit = require('../lib/unit');
+
+  // Operations
+  it('handles math operations between two units', function(){
+    // Addition between same units
+    expect(
+      new Unit(10, 'px').plus(new Unit(13, 'px'))
+    ).toEqual(
+      new Unit(23, 'px')
+    );
+
+    // Subtaction
+    expect(
+      new Unit(23, 'px').minus(new Unit(10, 'px'))
+    ).toEqual(
+      new Unit(13, 'px')
+    );
+
+    // Multiplication
+    expect(
+      new Unit(5, 'em').times(new Unit(4, 'em'))
+    ).toEqual(
+      new Unit(20, 'em')
+    );
+
+    // Division
+    expect(
+      new Unit(20, 'em').divide(new Unit(5, 'em'))
+    ).toEqual(
+      new Unit(4, 'em')
+    );
+  });
+});
