32c8244cc64ce00e60f736fa943f7ddab967743b
Jamen Marz <jamenmarz@gmail.com>
2015-11-18 22:55:28 -0700

    Add all essential files.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/* index.js
+ * Wrapper and accessor for library
+ */
+
+module.exports = exports = {};
+
+let lib = require('./lib');
+
+exports = lib.render;
+exports.prototype = lib;
